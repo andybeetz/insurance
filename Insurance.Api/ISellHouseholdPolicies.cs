7a1aca721d6a0acdc1136d7a76253f5cbd719044
Andy Beetz <andy@beetz.org.uk>
2026-02-01 14:53:37 +0000

    Add some documentation

--- Insurance.Api/ISellHouseholdPolicies.cs
+++ Insurance.Api/ISellHouseholdPolicies.cs
@@ -3,6 +3,9 @@ using Insurance.Api.Dtos.v1;
 
 namespace Insurance.Api;
 
+/// <summary>
+/// Abstraction that allows us to use Domain behaviour without cluttering the API endpoints
+/// </summary>
 public interface ISellHouseholdPolicies
 {
     Resulting<HouseholdPolicy> Sell(HouseholdPolicyDto policy);
