5ee3d7d4cbaa3cfc4733a1bee5d5a4cda68a2a8c
Andy Beetz <andy@beetz.org.uk>
2026-02-01 14:46:46 +0000

    Refactor Dtos

--- Insurance.Api/Dtos/v1/HouseholdPolicyDto.cs
+++ Insurance.Api/Dtos/v1/HouseholdPolicyDto.cs
@@ -2,19 +2,9 @@
 
 namespace Insurance.Api.Dtos.v1;
 
-public record HouseholdPolicyDto
+public record HouseholdPolicyDto : PolicyDto
 {
-    public Guid? UniqueReference { get; set; }
-    public required DateOnly StartDate { get; init; }
-    public required DateOnly EndDate { get; init; }
-    public required decimal Amount { get; init; }
-    public required bool HasClaims { get; init; }
-    public required bool AutoRenew { get; init; }
-    public required PolicyHolderDto PolicyHolder { get; init; }
-    public required PropertyDto Property { get; init; }
-    public required IReadOnlyCollection<PaymentDto> Payments { get; init; }
-
-    public HouseholdPolicy ToDomain() => new()
+    public override HouseholdPolicy ToDomain() => new()
     {
         UniqueReference = UniqueReference ?? Guid.Empty,
         StartDate = StartDate,
@@ -22,20 +12,20 @@ public record HouseholdPolicyDto
         Amount = Amount,
         HasClaims = HasClaims,
         AutoRenew = AutoRenew,
-        PolicyHolder = new PolicyHolder()
+        PolicyHolder = new PolicyHolder
         {
             FirstName = PolicyHolder.FirstName,
             LastName = PolicyHolder.LastName,
             DateOfBirth = PolicyHolder.DateOfBirth
         },
-        Property = new InsuredProperty()
+        Property = new InsuredProperty
         {
             AddressLine1 = Property.AddressLine1,
             AddressLine2 = Property.AddressLine2,
             AddressLine3 = Property.AddressLine3,
             PostCode = Property.PostCode
         },
-        Payments = Payments.Select(p => new PolicyPayment()
+        Payments = Payments.Select(p => new PolicyPayment
             { PaymentReference = p.PaymentReference, PaymentType = p.PaymentType, Amount = p.Amount }).ToList()
     };
 }
---
+++ Insurance.Api/Dtos/v1/PolicyDto.cs
@@ -0,0 +1,18 @@
+﻿using Insurance.Api.Domain;
+
+namespace Insurance.Api.Dtos.v1;
+
+public abstract record PolicyDto
+{
+    public Guid? UniqueReference { get; set; }
+    public required DateOnly StartDate { get; init; }
+    public required DateOnly EndDate { get; init; }
+    public required decimal Amount { get; init; }
+    public required bool HasClaims { get; init; }
+    public required bool AutoRenew { get; init; }
+    public required PolicyHolderDto PolicyHolder { get; init; }
+    public required PropertyDto Property { get; init; }
+    public required IReadOnlyCollection<PaymentDto> Payments { get; init; }
+    
+    public abstract Policy ToDomain();
+}
