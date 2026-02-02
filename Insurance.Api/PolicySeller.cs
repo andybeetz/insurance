fcbf32c4143d4624001cd3bfbdaaa82e2ffa6d63
Andy Beetz <andy@beetz.org.uk>
2026-02-02 16:29:40 +0000

    Refactor policy seller to address duplication

--- Insurance.Api/PolicySeller.cs
+++ Insurance.Api/PolicySeller.cs
@@ -8,66 +8,18 @@ public class PolicySeller : ISellPolicies
 {
     public Resulting<HouseholdPolicyDto> SellHouseholdPolicy(HouseholdPolicyDto policy)
     {
-        var periodResult = PolicyPeriod.Create(policy.StartDate, policy.EndDate);
-        if (!periodResult.IsSuccess)
-            return periodResult.Error!;
-
-        var moneyResult = Money.Create(policy.Amount);
-        if (!moneyResult.IsSuccess)
-            return moneyResult.Error!;
-
-        var policyHolders = new List<PolicyHolder>(policy.PolicyHolders.Count);
-        foreach (var h in policy.PolicyHolders)
-        {
-            var holderResult = PolicyHolder.Create(
-                firstName: h.FirstName,
-                lastName: h.LastName,
-                dateOfBirth: h.DateOfBirth);
-
-            if (!holderResult.IsSuccess)
-                return holderResult.Error!;
-
-            policyHolders.Add(holderResult.Value);
-        }
-
-        var holdersResult = PolicyHolders.Create(
-            policyHolders: policyHolders,
-            policyStartDate: periodResult.Value.StartDate);
-
-        if (!holdersResult.IsSuccess)
-            return holdersResult.Error!;
-
-        var propertyResult = InsuredProperty.Create(
-            addressLine1: policy.Property.AddressLine1,
-            addressLine2: policy.Property.AddressLine2,
-            addressLine3: policy.Property.AddressLine3,
-            postCode: policy.Property.PostCode);
-
-        if (!propertyResult.IsSuccess)
-            return propertyResult.Error!;
-
-        var payments = new List<PolicyPayment>(policy.Payments.Count);
-        foreach (var p in policy.Payments)
-        {
-            var paymentResult = PolicyPayment.Create(
-                paymentReference: p.PaymentReference,
-                paymentType: p.PaymentType,
-                amount: p.Amount);
-
-            if (!paymentResult.IsSuccess)
-                return paymentResult.Error!;
-
-            payments.Add(paymentResult.Value);
-        }
+        var domainInput = MapSellInput(policy);
+        if (!domainInput.IsSuccess)
+            return domainInput.Error!;
 
         var soldResult = HouseholdPolicy.Sell(
-            period: periodResult.Value,
-            amount: moneyResult.Value,
+            period: domainInput.Value.Period,
+            amount: domainInput.Value.Amount,
             hasClaims: policy.HasClaims,
             autoRenew: policy.AutoRenew,
-            policyHolders: holdersResult.Value,
-            property: propertyResult.Value,
-            payments: payments);
+            policyHolders: domainInput.Value.PolicyHolders,
+            property: domainInput.Value.Property,
+            payments: domainInput.Value.Payments);
 
         if (!soldResult.IsSuccess)
             return soldResult.Error!;
@@ -79,6 +31,30 @@ public class PolicySeller : ISellPolicies
     }
 
     public Resulting<BuyToLetPolicyDto> SellBuyToLetPolicy(BuyToLetPolicyDto policy)
+    {
+        var domainInput = MapSellInput(policy);
+        if (!domainInput.IsSuccess)
+            return domainInput.Error!;
+
+        var soldResult = BuyToLetPolicy.Sell(
+            period: domainInput.Value.Period,
+            amount: domainInput.Value.Amount,
+            hasClaims: policy.HasClaims,
+            autoRenew: policy.AutoRenew,
+            policyHolders: domainInput.Value.PolicyHolders,
+            property: domainInput.Value.Property,
+            payments: domainInput.Value.Payments);
+
+        if (!soldResult.IsSuccess)
+            return soldResult.Error!;
+
+        return Resulting<BuyToLetPolicyDto>.Success(policy with
+        {
+            UniqueReference = soldResult.Value.UniqueReference
+        });
+    }
+
+    private static Resulting<SellInput> MapSellInput(PolicyDto policy)
     {
         var periodResult = PolicyPeriod.Create(policy.StartDate, policy.EndDate);
         if (!periodResult.IsSuccess)
@@ -132,21 +108,18 @@ public class PolicySeller : ISellPolicies
             payments.Add(paymentResult.Value);
         }
 
-        var soldResult = BuyToLetPolicy.Sell(
-            period: periodResult.Value,
-            amount: moneyResult.Value,
-            hasClaims: policy.HasClaims,
-            autoRenew: policy.AutoRenew,
-            policyHolders: holdersResult.Value,
-            property: propertyResult.Value,
-            payments: payments);
-
-        if (!soldResult.IsSuccess)
-            return soldResult.Error!;
-
-        return Resulting<BuyToLetPolicyDto>.Success(policy with
-        {
-            UniqueReference = soldResult.Value.UniqueReference
-        });
+        return Resulting<SellInput>.Success(new SellInput(
+            periodResult.Value,
+            moneyResult.Value,
+            holdersResult.Value,
+            propertyResult.Value,
+            payments));
     }
+
+    private sealed record SellInput(
+        PolicyPeriod Period,
+        Money Amount,
+        PolicyHolders PolicyHolders,
+        InsuredProperty Property,
+        IReadOnlyCollection<PolicyPayment> Payments);
 }
