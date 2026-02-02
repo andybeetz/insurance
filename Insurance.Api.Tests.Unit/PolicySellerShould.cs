fde56b0762f8ec908010f81e4b8d6f1fbcb03bcb
Andy Beetz <andy@beetz.org.uk>
2026-02-02 16:03:59 +0000

    Create a test that a buy to let policy can be sold

---
+++ Insurance.Api.Tests.Unit/PolicySellerShould.cs
@@ -0,0 +1,34 @@
+﻿using Insurance.Api.Dtos.v1;
+
+namespace Insurance.Api.Tests.Unit;
+
+[TestFixture]
+public class PolicySellerShould
+{
+    [Test]
+    public void SellANewBuyToLetPolicy()
+    {
+        var newPolicyRequest = new BuyToLetPolicyDto
+        {
+            UniqueReference = Guid.Empty,
+            Amount = 50.00m,
+            AutoRenew = false,
+            HasClaims = false,
+            StartDate = DateOnly.FromDateTime(DateTime.UtcNow.Date),
+            EndDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(1)),
+            Payments = [new PaymentDto { PaymentReference = Guid.NewGuid(), PaymentType = "Card", Amount = 50.00m }],
+            Property = new PropertyDto
+                { AddressLine1 = "1 Test Street", AddressLine2 = null, AddressLine3 = null, PostCode = "ZZ1 1ZZ" },
+            PolicyHolder = new PolicyHolderDto
+            {
+                DateOfBirth = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(-30)), FirstName = "Test",
+                LastName = "User"
+            }
+        };
+        var policySeller = new PolicySeller();
+        
+        var policyResult = policySeller.SellBuyToLetPolicy(newPolicyRequest);
+        
+        Assert.That(policyResult.IsSuccess, Is.True);
+    }
+}
---
+++ Insurance.Api/PolicySeller.cs
@@ -0,0 +1,18 @@
+﻿using Insurance.Api.Dtos.v1;
+using Insurance.Api.Interfaces;
+using Insurance.Domain;
+
+namespace Insurance.Api;
+
+public class PolicySeller : ISellPolicies
+{
+    public Resulting<HouseholdPolicyDto> SellHouseholdPolicy(HouseholdPolicyDto policy)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Resulting<BuyToLetPolicyDto> SellBuyToLetPolicy(BuyToLetPolicyDto policy)
+    {
+        throw new NotImplementedException();
+    }
+}
