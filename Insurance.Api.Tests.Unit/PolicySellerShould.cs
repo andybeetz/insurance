c41d6acc4d302dc1cca35726b1af88c4e014279c
Andy Beetz <andy@beetz.org.uk>
2026-02-02 16:15:04 +0000

    Add a test to ensure domain errors are bubbling up

--- Insurance.Api.Tests.Unit/PolicySellerShould.cs
+++ Insurance.Api.Tests.Unit/PolicySellerShould.cs
@@ -35,4 +35,42 @@ public class PolicySellerShould
         
         Assert.That(policyResult.IsSuccess, Is.True);
     }
+    
+    /// <summary>
+    /// Makes sure that domain errors are bubbling up, the real tests exist elsewhere, and I won't re-test them here
+    /// </summary>
+    [Test]
+    public void NotSellABuyToLetPolicyWithAYoungPolicyHolder()
+    {
+        var newPolicyRequest = new BuyToLetPolicyDto
+        {
+            UniqueReference = Guid.Empty,
+            Amount = 50.00m,
+            AutoRenew = false,
+            HasClaims = false,
+            StartDate = DateOnly.FromDateTime(DateTime.UtcNow.Date),
+            EndDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(1)),
+            Payments = [new PaymentDto { PaymentReference = Guid.NewGuid(), PaymentType = "Card", Amount = 50.00m }],
+            Property = new PropertyDto
+                { AddressLine1 = "1 Test Street", AddressLine2 = null, AddressLine3 = null, PostCode = "ZZ1 1ZZ" },
+            PolicyHolders =
+            [
+                new PolicyHolderDto
+                {
+                    DateOfBirth = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(-15)),
+                    FirstName = "Test",
+                    LastName = "User"
+                }
+            ]
+        };
+        var policySeller = new PolicySeller();
+        
+        var policyResult = policySeller.SellBuyToLetPolicy(newPolicyRequest);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(policyResult.IsSuccess, Is.False);
+            Assert.That(policyResult.Error!.Code, Is.EqualTo("policy.holders.age.tooYoung"));
+            Assert.That(policyResult.Error.Description, Is.Not.Null.Or.Empty);
+        }
+    }
 }
