7d720eab9238c50084d07824bf24a6e011f8c9d1
Andy Beetz <andy@beetz.org.uk>
2026-02-02 16:22:00 +0000

    Sell a new household policy

--- Insurance.Api.Tests.Unit/PolicySellerShould.cs
+++ Insurance.Api.Tests.Unit/PolicySellerShould.cs
@@ -32,6 +32,7 @@ public class PolicySellerShould
         var policySeller = new PolicySeller();
         
         var policyResult = policySeller.SellBuyToLetPolicy(newPolicyRequest);
+        
         using (Assert.EnterMultipleScope())
         {
             Assert.That(policyResult.IsSuccess, Is.True);
@@ -69,11 +70,47 @@ public class PolicySellerShould
         var policySeller = new PolicySeller();
         
         var policyResult = policySeller.SellBuyToLetPolicy(newPolicyRequest);
+        
         using (Assert.EnterMultipleScope())
         {
             Assert.That(policyResult.IsSuccess, Is.False);
             Assert.That(policyResult.Error!.Code, Is.EqualTo("policy.holders.age.tooYoung"));
             Assert.That(policyResult.Error.Description, Is.Not.Null.Or.Empty);
+        }
+    }
+    
+    [Test]
+    public void SellANewHouseholdPolicy()
+    {
+        var newPolicyRequest = new HouseholdPolicyDto
+        {
+            UniqueReference = Guid.Empty,
+            Amount = 50.00m,
+            AutoRenew = false,
+            HasClaims = false,
+            StartDate = DateOnly.FromDateTime(DateTime.UtcNow.Date),
+            EndDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(1)),
+            Payments = [new PaymentDto { PaymentReference = Guid.NewGuid(), PaymentType = "Card", Amount = 50.00m }],
+            Property = new PropertyDto
+                { AddressLine1 = "1 Test Street", AddressLine2 = null, AddressLine3 = null, PostCode = "ZZ1 1ZZ" },
+            PolicyHolders =
+            [
+                new PolicyHolderDto
+                {
+                    DateOfBirth = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(-30)),
+                    FirstName = "Test",
+                    LastName = "User"
+                }
+            ]
+        };
+        var policySeller = new PolicySeller();
+        
+        var policyResult = policySeller.SellHouseholdPolicy(newPolicyRequest);
+        
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(policyResult.IsSuccess, Is.True);
+            Assert.That(policyResult.Value.UniqueReference, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
--- Insurance.Api/PolicySeller.cs
+++ Insurance.Api/PolicySeller.cs
@@ -8,7 +8,74 @@ public class PolicySeller : ISellPolicies
 {
     public Resulting<HouseholdPolicyDto> SellHouseholdPolicy(HouseholdPolicyDto policy)
     {
-        throw new NotImplementedException();
+        var periodResult = PolicyPeriod.Create(policy.StartDate, policy.EndDate);
+        if (!periodResult.IsSuccess)
+            return periodResult.Error!;
+
+        var moneyResult = Money.Create(policy.Amount);
+        if (!moneyResult.IsSuccess)
+            return moneyResult.Error!;
+
+        var policyHolders = new List<PolicyHolder>(policy.PolicyHolders.Count);
+        foreach (var h in policy.PolicyHolders)
+        {
+            var holderResult = PolicyHolder.Create(
+                firstName: h.FirstName,
+                lastName: h.LastName,
+                dateOfBirth: h.DateOfBirth);
+
+            if (!holderResult.IsSuccess)
+                return holderResult.Error!;
+
+            policyHolders.Add(holderResult.Value);
+        }
+
+        var holdersResult = PolicyHolders.Create(
+            policyHolders: policyHolders,
+            policyStartDate: periodResult.Value.StartDate);
+
+        if (!holdersResult.IsSuccess)
+            return holdersResult.Error!;
+
+        var propertyResult = InsuredProperty.Create(
+            addressLine1: policy.Property.AddressLine1,
+            addressLine2: policy.Property.AddressLine2,
+            addressLine3: policy.Property.AddressLine3,
+            postCode: policy.Property.PostCode);
+
+        if (!propertyResult.IsSuccess)
+            return propertyResult.Error!;
+
+        var payments = new List<PolicyPayment>(policy.Payments.Count);
+        foreach (var p in policy.Payments)
+        {
+            var paymentResult = PolicyPayment.Create(
+                paymentReference: p.PaymentReference,
+                paymentType: p.PaymentType,
+                amount: p.Amount);
+
+            if (!paymentResult.IsSuccess)
+                return paymentResult.Error!;
+
+            payments.Add(paymentResult.Value);
+        }
+
+        var soldResult = HouseholdPolicy.Sell(
+            period: periodResult.Value,
+            amount: moneyResult.Value,
+            hasClaims: policy.HasClaims,
+            autoRenew: policy.AutoRenew,
+            policyHolders: holdersResult.Value,
+            property: propertyResult.Value,
+            payments: payments);
+
+        if (!soldResult.IsSuccess)
+            return soldResult.Error!;
+
+        return Resulting<HouseholdPolicyDto>.Success(policy with
+        {
+            UniqueReference = soldResult.Value.UniqueReference
+        });
     }
 
     public Resulting<BuyToLetPolicyDto> SellBuyToLetPolicy(BuyToLetPolicyDto policy)
